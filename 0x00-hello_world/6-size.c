661ddd870aed3f18faf7e9529c0701bf01f64894
Emmanuel <aguemmanuel221@gmail.com>
2022-09-12 02:37:56 -0700

    jdvc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ sizeof(char));
 printf("Size of an int: %i byte(s)\n",                            sizeof(int));
 printf("Size of a long int: %i byte(s)\n",                        sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n",                   sizeof(long long int));
-printf("Size of a float: %i byte(s)\n",                           sizeof(foat));
+printf("Size of a float: %i byte(s)\n",                           sizeof(float));
 return (0);
 }
