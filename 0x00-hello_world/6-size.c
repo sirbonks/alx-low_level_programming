0affe60cf9871ec33c67adfc7041455eee1edbb2
Emmanuel <aguemmanuel221@gmail.com>
2022-09-12 14:01:40 -0700

    hgee

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - print the size of type chracters
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
+	
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
         printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
