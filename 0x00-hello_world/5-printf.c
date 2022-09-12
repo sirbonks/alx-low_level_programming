1dba25ac81244deb121eedfccdb36ceb500cf93d
Emmanuel <aguemmanuel221@gmail.com>
2022-09-12 01:47:30 -0700

    kgs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  *main - main block
- *Reture: 0
+ *Return: 0
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of	 art,\n");
-	Return (0);
+	return (0);
 }
