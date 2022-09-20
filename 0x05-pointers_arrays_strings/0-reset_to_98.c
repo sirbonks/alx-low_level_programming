aa8fc933dacd028f90b531a4a7419e0fd6e2bf11
Emmanuel <aguemmanuel221@gmail.com>
2022-09-19 17:42:50 -0700

    kdx

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of points nto 98
+ * reset_to_98 - updates the value of points n to 98
  *
  * @n: input
  * Return: void
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -5,7 +5,7 @@
  * @c: the character to print
  *
  * Return: on success 1
- * on error, -1 is returned, and error is set appropriately
+ * on error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
