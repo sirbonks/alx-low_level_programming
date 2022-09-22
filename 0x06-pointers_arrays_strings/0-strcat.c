1706496a2cd9f4e69aac0872f6d8fb78be4f0be3
Emmanuel <aguemmanuel221@gmail.com>
2022-09-22 13:47:51 -0700

    bghf

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.hh"
+
+/**
+ * _strcat - joins two strings
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	while (dest[1] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
