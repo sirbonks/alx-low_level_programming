c5195f158bc17754c24df01eb5cdb8c1b1e2ccdc
Emmanuel <aguemmanuel221@gmail.com>
2022-09-22 14:53:36 -0700

    jgc

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: input
+ * @src: input
+ * @n: number of char
+ * Return: void
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
