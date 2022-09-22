0bcd91f0fa5fcf59d5c8e3d6b050731c117a7f32
Emmanuel <aguemmanuel221@gmail.com>
2022-09-22 15:27:44 -0700

    hjfgy

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to upper
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
