b440354a04dd3b81ba0a4c2a4c4a5663a553d86b
Emmanuel <aguemmanuel221@gmail.com>
2022-09-22 16:08:59 -0700

    jhdfg

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337
+ * @n: input
+ * Return: value of n
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char A1[] = "aAeoOtTlL";
+	char A2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == A1[j])
+			{
+				n[i] = A2[j];
+			}
+		}
+	}
+	return (n);
+}
